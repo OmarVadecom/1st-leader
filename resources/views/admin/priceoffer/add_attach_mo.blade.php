390a6a93ca40dc23551319137a129ff0fe530198
eslam-vadecomeslam-vadecomeslam-vadecomgit config --global user.name eslam-vadecomgit config user.email MY_NAME@example.comgit config user.email MY_NAME@example.comgit config user.email islam.ibrahim@vadecom.net <islam.ibrahim@vadecom.net>
2022-04-25 13:32:05 +0200

    add  and delete attachment button

--- resources/views/admin/priceoffer/add_attach_mo.blade.php
+++ resources/views/admin/priceoffer/add_attach_mo.blade.php
@@ -20,26 +20,73 @@
     <br>
     <div class="row">
         <div class="col-md-3"></div>
-        <div class="col-md-6">
+        <div class="col-md-6 ">
             <form action="{{route('priceoffer.multiuploads',$offer->id)}}" method="post" enctype="multipart/form-data">
                 @csrf
-                <label for="">اضافة مرفقات :</label>
-                <br />
-                <select name="type">
-                    <option disabled selected>اختر نوع المرفق</option>
-                    <option value="كفالة غرم">كفالة غرم</option>
-                    <option value=" كمبياله"> كمبياله</option>
-                    <option value=" بيع اجل"> بيع اجل</option>
-                    <option value=" كروكى "> كروكى </option>
-                </select>
-                <br>
-                <br>
-                <input type="file" class="form-control" name="attach" multiple />
-                <br /><br />
-                <input type="submit" class="btn btn-primary" value="Upload" />
+
+                <div class="showattach" id="showAttach">
+                    <div class="d-flex">
+                        <label for="">اضافة مرفقات :</label>
+                    <br />
+                    <button type="button" onclick="addAttachment()" class="btn btn-primary ">+</button>
+                    <select name="type">
+                        <option disabled selected>اختر نوع المرفق</option>
+                        <option value="كفالة غرم">كفالة غرم</option>
+                        <option value=" كمبياله"> كمبياله</option>
+                        <option value=" بيع اجل"> بيع اجل</option>
+                        <option value=" كروكى "> كروكى </option>
+                    </select>
+                    </div>
+                    <br>
+                    <br>
+                    <input type="file" class="form-control" name="attach" multiple />
+                    <br /><br />
+                    <input type="submit" class="btn btn-primary" value="Upload" />
+                </div>
             </form>
         </div>
     </div>
 </div>
 
+
+
 @endsection
+
+
+<script>
+function addAttachment()
+{
+
+    $("#showAttach").append(`
+                    <div>
+                    <div class="Attachment d-flex"  style="margin-top: 10px;">
+                        <label >اضافة مرفقات :</label>
+                    <br />
+                    <button type="button" onclick="removeAttachment(this)" class="btn btn-primary ">-</button>
+                    <button type="button" onclick="addAttachment()" class="btn btn-primary ">+</button>
+                    <select name="type">
+                        <option disabled selected>اختر نوع المرفق</option>
+                        <option value="كفالة غرم">كفالة غرم</option>
+                        <option value=" كمبياله"> كمبياله</option>
+                        <option value=" بيع اجل"> بيع اجل</option>
+                        <option value=" كروكى "> كروكى </option>
+                    </select>
+                    </div>
+                    <br>
+                    <br>
+                    <input type="file" class="form-control" name="attach" multiple />
+                    <br /><br />
+                    <input type="submit" class="btn btn-primary my-4" value="Upload" />
+                    </div>
+                `);
+}
+function removeAttachment(data)
+{
+    $(data).parent().parent().remove();
+}
+</script>
+
+
+
+
+{{-- @include('admin.layouts.script') --}}
